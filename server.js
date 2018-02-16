d5ce1c7ed4321691d3032f9f8e024deb9efb6eac
Jay Winstead <jwinstead@launchdarkly.com>
2018-02-16 12:11:11 -0800

    add airtable

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 // Initialize using verification token from environment variables
+var airTable = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base(process.env.AIRTABLE_BASE);
+
 const createSlackEventAdapter = require('@slack/events-api').createSlackEventAdapter;
 const slackEvents = createSlackEventAdapter(process.env.SLACK_VERIFICATION_TOKEN);
 const port = process.env.PORT || 3000;
 
 // Attach listeners to events by Slack Event "type". See: https://api.slack.com/events/message.im
 slackEvents.on('reaction_added', (event) => {
-  console.log(JSON.stringify(event));
+    airTable('Table 1').create(event, function(err, record) {
+      if (err) { console.error(err); return; }
+      console.log(record.getId());
+  });
 });
 
 // Handle errors (see `errorCodes` export)
