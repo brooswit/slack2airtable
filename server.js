b00335561c40f130f8201f267fca77b624df7691
Jay Winstead <jwinstead@launchdarkly.com>
2018-02-16 10:09:05 -0800

    so many initial commits

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
-// Modules
+// Initialize using verification token from environment variables
+const createSlackEventAdapter = require('@slack/events-api').createSlackEventAdapter;
+const slackEvents = createSlackEventAdapter(process.env.SLACK_VERIFICATION_TOKEN);
+const port = process.env.PORT || 3000;
 
-var http = require('http');
-var path = require('path');
+// Attach listeners to events by Slack Event "type". See: https://api.slack.com/events/message.im
+slackEvents.on('message', (event) => {
+  console.log(`Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`);
+});
 
-// Config
+// Handle errors (see `errorCodes` export)
+slackEvents.on('error', console.error);
 
-// Startup
+// Start a basic HTTP server
+slackEvents.start(port).then(() => {
+  console.log(`server listening on port ${port}`);
+});
