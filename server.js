1f6ecadf90aa7c4e9a3cf588aed6f447d169f443
Jay Winstead <jwinstead@launchdarkly.com>
2018-02-16 15:20:11 -0800

    cleanup

--- server.js
+++ server.js
@@ -13,8 +13,6 @@ const slackApi = new WebClient(process.env.SLACK_AUTH_TOKEN);
 // Attach listeners to events by Slack Event "type". See: https://api.slack.com/events/message.im
 slackEvents.on('reaction_added', (event) => {
   var reaction = event.reaction
-  console.log("channel: " + event.item.channel)
-  console.log("ts: " + event.item.ts)
   slackApi.reactions.get({
     channel: event.item.channel,
     timestamp: event.item.ts
@@ -25,7 +23,7 @@ slackEvents.on('reaction_added', (event) => {
     slackApi.channels.info(event.item.channel).then((res)=>{
       var channel = res.channel.name;
 
-      slackApi.users.info().then((res)=>{
+      slackApi.users.info(event.user).then((res)=>{
         var user = res.user.profile.real_name || res.user.name;
 
         var payload = {
